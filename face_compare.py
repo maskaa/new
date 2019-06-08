2a06a60004975b9063d59c0a7723beab3ed81c9d
maskaa <51506154+maskaa@users.noreply.github.com>
2019-06-08 21:53:39 +0300

    Update face_compare.py

--- face_compare.py
+++ face_compare.py
@@ -2,22 +2,20 @@ import glob
 import face_recognition
 
 def face_compare_func(img1, img2):
-    img1 = 'photos/50210887.jpg'
-    img2 = 'photos/50210887.jpg'
-    face = face_recognition.load_image_file('photos/50210887.jpg')
+    face = face_recognition.load_image_file(img1)
     face = face_recognition.face_encodings(face)[0]
-    unknown_picture = face_recognition.load_image_file('photos/50210887.jpg')
+    unknown_picture = face_recognition.load_image_file(img2)
     unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
     results = face_recognition.compare_faces([face], unknown_face_encoding)
-    
     if results[0] == True:
-        print('-----------------------------------------------------')
+        return True
     else:
-        print('++++++++++++++++++++++++++++++++++++++++++++++++++++')
+        return False
 
 def open_files(img):
-    for filename in glob.glob('registered_photos/' + '*.jpg'):
+    for filename in sorted(glob.glob('/home/maska/Documents/hakaton_fbr/registered_photos/' + '*.jpg')):
         filename = filename
-        print(face_compare_func(filename, img))
+        print(filename)
+        #print(face_compare_func(filename, img))
 
-face_compare_func(1, 1)
+print(open_files("/home/maska/Documents/hakaton_fbr/registered_photos/11111.jpg"))
