db9ec1d3c53cd57ccdeaf144d3de9495905d3854
maskaa <51506154+maskaa@users.noreply.github.com>
2019-06-09 01:03:53 +0300

    Update face_compare.py

--- face_compare.py
+++ face_compare.py
@@ -1,6 +1,7 @@
 import glob
 import face_recognition
 
+
 def face_compare_func(img1, img2):
     face = face_recognition.load_image_file(img1)
     face = face_recognition.face_encodings(face)[0]
@@ -9,14 +10,12 @@ def face_compare_func(img1, img2):
     results = face_recognition.compare_faces([face], unknown_face_encoding)
     if results[0] == True:
         print("https://vk.com/id"+img2[52:-4])
-        return True
     else:
         return False
     
 
 def open_files(img):
     for filename in sorted(glob.glob('/home/maska/Documents/hakaton_fbr/registered_photos/' + '*.jpg')):
-        filename = filename
         print(face_compare_func(filename, img))
 
-print(open_files("/home/maska/Documents/hakaton_fbr/registered_photos/11111.jpg"))
+print(open_files("/home/maska/Documents/hakaton_fbr/photos/11111.jpg"))
