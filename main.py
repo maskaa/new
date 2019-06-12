78c65b2352b288c3009a3aabefc6ca9ed197e87f
maskaa <51506154+maskaa@users.noreply.github.com>
2019-06-12 09:36:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from vk_bot import VkBot
 from vk_api.utils import get_random_id
 
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message['text'], 'keyboard': message['keyboard'], 'random_id': get_random_id()})
-    user_url = "https://vk.com/id" + str(user_id)
-    if message['find'] == True:
-	    vk.method('messages.send', {'user_id': message['id'], 'message': 'Вас нашел этот человек '+'{}'.format(user_url), 'keyboard': None, 'random_id': get_random_id()})
+    try:
+        vk.method('messages.send', {'user_id': user_id, 'message': message['text'], 'keyboard': message['keyboard'], 'random_id': get_random_id()})
+        user_url = "https://vk.com/id" + str(user_id)
+        if 'find' in message:
+            vk.method('messages.send', {'user_id': message['id'], 'message': 'Вас нашел этот человек '+'{}'.format(user_url), 'keyboard': None, 'random_id': get_random_id()})
+    except:
+        print('Some problems with server')
+        return 0
 
 token = ""
 
@@ -21,6 +25,6 @@ for event in longpoll.listen():
         if event.to_me:
             print('New message:')
             print(f'For me by: {event.user_id}')
-
+            print(event)
             bot = VkBot(event.user_id)
             write_msg(event.user_id, bot.new_message(event))
